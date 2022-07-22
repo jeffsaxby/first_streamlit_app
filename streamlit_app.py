0d74fc510cca93b2b7ef53259e4020e33fa4b508
Jeff Saxby <109544732+jeffsaxby@users.noreply.github.com>
2022-07-22 08:15:12 -0400

    Update streamlit_app.py
    add a write back to db

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,5 +43,9 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+# let the user enter their own fruit choice
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+# write fruit choice to the db
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
